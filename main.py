0020a34d176ccde6d91ace315262317f904ffa3f
Akki <68499086+AkshaySK20@users.noreply.github.com>
2023-09-08 21:07:33 +0530

    Progress bar inclusion added

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import tkinter
 import customtkinter
 from pytube import YouTube
+import threading
+import time
 
 def download():
     try:
         ytLink = link.get()
-        ytObject = YouTube(ytLink)
+        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
         video = ytObject.streams.get_highest_resolution()
+
+        title.configure(text=ytObject.title, text_color="white")
+        finishLabel.configure(text="")
+
         video.download()
+        finishLabel.configure(text="Finished Downloading!")
+
     except:
-        print("YouTube link is invalid")
-    print("Download Complete!")
+        finishLabel.configure(text="Error! YouTube link is Invalid.", text_color="red")
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = bytes_downloaded/total_size * 100
+    per = str(int(percentage_of_completion))
+    progPercentage.configure(text=per +'%')
+    progPercentage.update()
+
+    # update progressbar
+    progressBar.set(float(percentage_of_completion)/100)
+
 
 # System settings
 customtkinter.set_appearance_mode("System")
@@ -30,13 +49,24 @@ url_var = tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
 link.pack()
 
+# Finished Downloading
+finishLabel = customtkinter.CTkLabel(app, text="")
+finishLabel.pack()
+
+# Progress Bar
+progPercentage = customtkinter.CTkLabel(app, text="0%")
+progPercentage.pack()
+
+progressBar = customtkinter.CTkProgressBar(app, width=400)
+progressBar.set(0)
+progressBar.pack(padx=10, pady=10)
+
 # Download button
 download = customtkinter.CTkButton(app, text="Download", command=download)
 download.pack(padx=10, pady=10)
 
+
 # Run app
 app.mainloop()
-
-# Progress Bar
 
 
